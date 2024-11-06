1a4d0b5a762be017f0cc511ba3133e9db5d338f5
Obayuwana22 <successobayuwana@gmail.com>
2024-11-06 18:00:17 +0100

    New update to form two

--- src/components/stepperForms/FormTwo.jsx
+++ src/components/stepperForms/FormTwo.jsx
@@ -189,6 +189,7 @@ const FormTwo = ({ onChange, value, subHead, endDate, showSwitch }) => {
                 <li
                   key={index}
                   onClick={() => handleOptionClick("roleTitle", dropdown)}
+                   className="px-4 py-2"
                 >
                   {dropdown}
                 </li>
@@ -236,11 +237,12 @@ const FormTwo = ({ onChange, value, subHead, endDate, showSwitch }) => {
           </div>
 
           {isOpen.seniorityLevel && (
-            <ul className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-auto">
+            <ul className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-auto ">
               {Dropdown.seniorityLevels.map((dropdown, index) => (
                 <li
                   key={index}
                   onClick={() => handleOptionClick("seniorityLevel", dropdown)}
+                   className="px-4 py-2"
                 >
                   {dropdown}
                 </li>
