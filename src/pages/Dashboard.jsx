90dd34c6f3aca7b62d1f984f7d2acb522fa2690a
Obayuwana22 <successobayuwana@gmail.com>
2024-12-30 20:13:49 +0100

    Fix responsive bug

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div className="font-poppins md:mx-auto md:flex">
       <Navbar />
       {/* <main className="flex-1 min-h-[844px] 2xl:max-w-[1512px] 2xl:max-h-[1024px]"> */}
-      <main className="flex-1 2xl:max-h-[1024px] ml-0 md:ml-[228px]">
+      <main className="flex-1 max-h-[1024px] ml-0 md:ml-[228px]">
         <Outlet />
       </main>
     </div>
