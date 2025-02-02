9953c5c5388368fe5f3d81d9aed6d5ed7f7f026d
Joxeph <codewithjoxeph@gmail.com>
2025-02-02 15:31:58 +0100

    env

--- src/components/agreements/AgreementTable.jsx
+++ src/components/agreements/AgreementTable.jsx
@@ -17,27 +17,7 @@ const AgreementTable = () => {
   ];
   return (
     <section>
-      <div>
-        <div className=" font-semibold mt-2 mb-5 xl:text-xl xl:mt-0 xl:mb-10">
-          Overall Client's Agreements
-        </div>
-        <div className="flex items-center justify-between font-medium mt-2 lg:justify-self-start xl:text-xl">
-          <div className="flex items-center gap-1 md:mr-[21px]">
-            Active Developers
-            <div
-              className="w-3 h-3 rounded-full "
-              style={{ backgroundColor: "#008000" }}
-            ></div>
-          </div>
-          <div>
-            <SearchInput />
-          </div>
-        </div>
-
-        <div>
-          <AgreementsCard />
-        </div>
-      </div>
+     
     </section>
   );
 };
