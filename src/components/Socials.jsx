4566b8f70b5730d3d5c8c00b8d5b8cbaa9164be6
Clinton <110818591+clinton401@users.noreply.github.com>
2025-09-12 22:41:55 +0100

    Feature/socials (#22)
    * Add Socials component with OAuth loading state
    
    * added oauth for github
    
    * removed console.log statements
    
    * fixed the oauth button
    
    * increased the socials loader animation speed

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -39,28 +39,31 @@ const Socials = ({ isRegisterPage = false }) => {
         
 
         {loadingProvider === "github" ? (
-          <span className="">
-            <svg
-              className="animate-spin h-5 w-5 text-gray-600"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
-              ></path>
-            </svg>
-          </span>
+
+         <span>
+  <svg
+    className="animate-spin h-5 w-5 text-gray-600 [animation-duration:0.5s]"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    />
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
+    />
+  </svg>
+</span>
+
+
         ): (
           <>
           <img src={GitHubLogo} alt="github logo" className="w-6 h-6" />
@@ -79,28 +82,30 @@ const Socials = ({ isRegisterPage = false }) => {
        
 
         {loadingProvider === "google" ? (
-          <span className="">
-            <svg
-              className="animate-spin h-5 w-5 text-gray-600"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
-              ></path>
-            </svg>
-          </span>
+         <span>
+  <svg
+    className="animate-spin h-5 w-5 text-gray-600 [animation-duration:0.5s]"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    />
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 100 16v-4l-3 3 3 3v-4a8 8 0 01-8-8z"
+    />
+  </svg>
+</span>
+
+
         ) : (
           <>
            <img src={googleLogo} alt="google logo" className="w-6 h-6"  />
